4e3ec3a490a2d521747bd71ae4dcb8adeb7ff6f7
Jun Yeon <ziririsky8@naver.com>
2026-08-07 17:29:48 +0900

    feat: implement member CRUD API with Spring Boot

--- src/main/java/com/example/spring_member/controller/MemberController.java
+++ src/main/java/com/example/spring_member/controller/MemberController.java
@@ -1,12 +1,20 @@
 package com.example.spring_member.controller;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.spring_member.dto.MemberDto;
 import com.example.spring_member.entity.Member;
 import com.example.spring_member.service.MemberService;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class MemberController {
@@ -22,4 +30,27 @@ public class MemberController {
             memberService.join(member);
             
 }   
-}
+    @GetMapping("/members")
+    public List<Member> findAll(){
+        return memberService.findAll();
+
+    }
+    @GetMapping("/members/{id}")
+    public Member findByID(@PathVariable Long id){
+        return memberService.findById(id);
+
+    }
+    @PutMapping("/members/{id}")
+    public Member update(@PathVariable Long id, @RequestBody MemberDto dto){
+         memberService.update(id, dto);
+         return memberService.findById(id);
+    }
+    @DeleteMapping("/members/{id}")
+    public void delete(@PathVariable Long id){
+        memberService.delete(id);
+        
+    }
+
+ }
+    
+    
--- src/main/java/com/example/spring_member/service/MemberService.java
+++ src/main/java/com/example/spring_member/service/MemberService.java
@@ -1,9 +1,14 @@
 package com.example.spring_member.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import com.example.spring_member.dto.MemberDto;
 import com.example.spring_member.entity.Member;
 import com.example.spring_member.repository.MemberRepository;
+
+import jakarta.transaction.Transactional;
 
 @Service
 public class MemberService {
@@ -16,5 +21,24 @@ public class MemberService {
 
         public void join(Member member){
             memberRepository.save(member);
+        }
+        public List<Member> findAll(){
+            return memberRepository.findAll();
+
+        }
+        public Member findById(Long id){
+           return memberRepository.findById(id).orElseThrow();
+           
+        }
+        @Transactional
+        public void update(Long id, MemberDto dto){
+            Member member = findById(id);
+            member.setName(dto.getName());
+
+        }
+        @Transactional
+        public void delete(Long id){
+            Member member = findById(id);
+            memberRepository.delete(member);
         }
 }
